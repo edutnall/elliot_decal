b7021cca382ffd8e292d9efcc5963ba75e1cc0ad
Elliot Dutnall <edutnall@berkeley.edu>
2025-02-09 16:59:59 -0800

    it saved ???

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,23 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+    # The command cd ~/Desktop with no tags or descriptors attached would send you directly to the desktop.
 # 2) What does ~/ mean?
-# 3) What's the difference between and abolute and a relative path in your terminal?
+    # ~ means the entire drive in which you are working from in your terminal, like C: on Windows.
+# 3) What's the difference between an absolute and a relative path in your terminal?
+    # An absolute path to a directory runs from your root directory, whilst a relative path is calculated from the current working directory.
 # 4) What command would bring you back to your home directory?
-# 5) If you called rm ./ in your current working directory, what would it do? 
+    # Calling cd without any other modifiers would bring you back to your home directory.
+# 5) If you called rm ./ in your current working directory, what would it do?
+    # Calling rm ./ deletes every file and directory within and under your current working directory.
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+    # It does nothing, since I am not in the directory with git functionality enabled, /elliot_decal. 
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+    # The message that appears is "fatal: not a git repository (or any of the parent directories): .git", which is telling me that I cannot use git functions 
+    # in this directory because it is not a git repository.
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+    # The most frustrating bug or error I've encountered in this class so far was when I tried to link my directory to github, and I troubleshooted and resolved
+    # it by creating a new directory and activating github in there instead, which worked somehow.  
 # 9) How does cloning a repository differ from pulling from a repository?
+    # Cloning a repository is essentially copying the entire contents of that repository onto your computer, while pulling from a repository merely updates the 
+    # contents to the most recent published version.
 # 10) Tell me a fun fact!
+    # I really like reading about nuclear astrophysics, nucleosynthesis and nuclear fusion.
